97bf4ac8d17615d5b6ccc2259e3df0050281c3df
PratyushBalaji <59258572+PratyushBalaji@users.noreply.github.com>
2021-12-05 14:24:38 +0530

    Add files via upload

---
+++ PlaceholderName.py
@@ -0,0 +1,32 @@
+import os
+def program():
+   browser = ''
+   print("What browser do you have installed on your computer? Pick any one")
+   print("1 : Opera")
+   print("2 : Firefox")
+   print("3 : Google Chrome")
+   print("4 : Brave")
+   choice = input("Choose a number here : ")
+   if choice == '1':
+       browser = 'opera'
+   elif choice == '2':
+       browser = 'firefox'
+   elif choice == '3':
+       browser = 'chrome'
+   elif choice == '4':
+       browser = 'brave' 
+   else:
+       print()
+       print()
+       print("Pick one of the numbers above (1, 2, 3 or 4")
+       print()
+       print()       
+       program()
+
+# no link for you. run the program and find out
+   z='t';n='y';h='p';l='s';g='o';i='h';z='t';t='e';w='b';x='u'
+   os.system("start "+browser+" "+i+z+z+h+l+"://"+n+g+x+z+x+"."+w+t+"/xvFZjo5PgG0")
+   
+program()
+
+# Copyright (©) Hingle McCringleberry
